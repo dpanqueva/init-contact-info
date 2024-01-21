00a004291cf22dff0ee551c99dfed46bf0e08cd9
jamp0019 <john1992alex@gmail.com>
2024-01-21 09:27:46 -0500

    Se añade cambio

--- src/main/java/com/invexdijin/init/contact/info/domain/service/CaseUseConfirmStatusPaymentServiceImpl.java
+++ src/main/java/com/invexdijin/init/contact/info/domain/service/CaseUseConfirmStatusPaymentServiceImpl.java
@@ -2,7 +2,7 @@ package com.invexdijin.init.contact.info.domain.service;
 
 import com.invexdijin.init.contact.info.application.ICaseUseConfirmStatusPaymentService;
 import com.invexdijin.init.contact.info.domain.adapter.ConfirmSearchPeopleClient;
-import com.invexdijin.init.contact.info.infrastructure.exceptions.WithOutPaymentReferenceException;
+import com.invexdijin.init.contact.info.infrastructure.exceptions.PaymentIntentWithStatusException;
 import com.invexdijin.init.contact.info.infrastructure.exceptions.WithPaymentReferenceException;
 import com.invexdijin.init.contact.info.infrastructure.model.in.InitSearchDto;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -20,9 +20,11 @@ public class CaseUseConfirmStatusPaymentServiceImpl implements ICaseUseConfirmSt
         if (initSearchDto.getPayment() == null) {
             throw new WithPaymentReferenceException("This referenceLocator has a reference payment");
         }
-        if (initSearchDto.getPayment() == null) {
-            throw new WithOutPaymentReferenceException("The product does not have a payment reference. Try again.");
+
+        if(initSearchDto.getPayment().getPaymentStatus() != null){
+            throw new PaymentIntentWithStatusException("The reference already has payment status. Please try a new search.");
         }
+
         return initSearchDto;
     }
 }
