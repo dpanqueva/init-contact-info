821ec9129f74748a8d1456b485a62a1bd8517244
Diego Panqueva <daerb9011@gmail.com>
2024-02-05 09:27:40 -0500

    Feat: Fix call to verifica.co and change configurations about fly.toml

--- src/main/java/com/invexdijin/init/contact/info/domain/adapter/ApiSearchPeopleClient.java
+++ src/main/java/com/invexdijin/init/contact/info/domain/adapter/ApiSearchPeopleClient.java
@@ -2,14 +2,15 @@ package com.invexdijin.init.contact.info.domain.adapter;
 
 import com.invexdijin.init.contact.info.domain.model.api1.Response;
 import com.invexdijin.init.contact.info.infrastructure.model.in.TriedSearchDto;
+import lombok.SneakyThrows;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
-import org.springframework.http.HttpHeaders;
-import org.springframework.http.MediaType;
-import org.springframework.http.ResponseEntity;
+import org.springframework.http.*;
 import org.springframework.stereotype.Component;
 import org.springframework.web.client.RestTemplate;
 import org.springframework.web.util.UriComponentsBuilder;
+
+import java.net.URI;
 
 @Component
 public class ApiSearchPeopleClient {
@@ -24,7 +25,7 @@ public class ApiSearchPeopleClient {
     @Value("${client.api1.token.key}")
     private String token;
 
-
+@SneakyThrows
     public Response consultSearchPeople(TriedSearchDto request) {
         HttpHeaders headers = new HttpHeaders();
         headers.setContentType(MediaType.APPLICATION_JSON);
@@ -33,9 +34,11 @@ public class ApiSearchPeopleClient {
                 .queryParam("documentType", request.getDocumentType())
                 .queryParam("documentNumber", request.getDocumentNumber())
                 .toUriString();
-        ResponseEntity<Response> response = client.getForEntity(finalUrl, Response.class, headers);
 
-        return response.getBody();
+        RequestEntity<?> requestEntity = new RequestEntity<>(headers, HttpMethod.GET, new URI(finalUrl));
+        ResponseEntity<Response> responseEntity = new RestTemplate().exchange(requestEntity, Response.class);
+
+        return responseEntity.getBody();
 
     }
 
