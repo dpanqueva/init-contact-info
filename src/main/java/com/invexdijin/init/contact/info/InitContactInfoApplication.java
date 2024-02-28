61e4dc8fc2787fc14c9b9cf2cc4f2656e8e0900f
jamp0019 <john1992alex@gmail.com>
2024-02-28 09:35:43 -0500

    Se añade anotación enableFeignClient en clase principal de ejecución

--- src/main/java/com/invexdijin/init/contact/info/InitContactInfoApplication.java
+++ src/main/java/com/invexdijin/init/contact/info/InitContactInfoApplication.java
@@ -2,8 +2,10 @@ package com.invexdijin.init.contact.info;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 
 @SpringBootApplication
+@EnableFeignClients
 public class InitContactInfoApplication {
 
 	public static void main(String[] args) {
